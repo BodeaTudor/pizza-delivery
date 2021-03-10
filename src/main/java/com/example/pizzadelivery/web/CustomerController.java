73f8df3beb2c9ec867ca5e0d016175f515d499b2
Bodea Tudor <bodea.tudor99@yahoo.com>
2021-03-10 13:08:00 +0200

    *added a GET endpoint for the CustomerController class

--- src/main/java/com/example/pizzadelivery/web/CustomerController.java
+++ src/main/java/com/example/pizzadelivery/web/CustomerController.java
@@ -6,10 +6,7 @@ import com.example.pizzadelivery.transfer.customer.SaveCustomerRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -30,5 +27,13 @@ public class CustomerController {
         Customer newCustomer = customerService.createCustomer(request);
 
         return new ResponseEntity<>(newCustomer, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id) {
+
+        Customer retrievedCustomer = customerService.getCustomer(id);
+
+        return new ResponseEntity<>(retrievedCustomer, HttpStatus.OK);
     }
 }
