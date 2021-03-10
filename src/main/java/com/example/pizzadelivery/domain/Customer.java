5ef1e48e2cbdf4df4de405903b352c9f0f1fb711
Bodea Tudor <bodea.tudor99@yahoo.com>
2021-03-10 12:21:32 +0200

    *added a new Customer class in domain layer

---
+++ src/main/java/com/example/pizzadelivery/domain/Customer.java
@@ -0,0 +1,24 @@
+package com.example.pizzadelivery.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+}
